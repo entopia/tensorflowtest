bd2ce85f2d43306cc06f4fbd2eaa700053afcee6
Zachary Price <zachary.price@agiledatum.com>
2019-12-11 16:22:16 +0000

    update objd.py

--- obj_detection/objd.py
+++ obj_detection/objd.py
@@ -1,4 +1,5 @@
 from os import listdir
+from os import path
 from xml.etree import ElementTree
 from numpy import zeros
 from numpy import asarray
@@ -29,32 +30,31 @@ class PlansDataset(Dataset):
 
 	def load_dataset(self, dataset_dir, is_train=True):
 		# define one class
-		self.add_class("dataset", 1, "plans")
-		
+		self.add_class("dataset", 1, "drawing")
+		self.add_class("dataset", 2, "drawingtitle")
+		self.add_class("dataset", 3, "blocktitle")
+
 		images_dir = dataset_dir + '/images/'
 		annotations_dir = dataset_dir + '/annots/'
 
-                # add each example to the dataset (image and xml boundary information)
-		for filename in listdir(images_dir):
+		num_examples = len(listdir(images_dir))
+		if is_train: # TODO: specify training size
+			data_files = listdir(images_dir)[:int(num_examples*0.8)]
+		else:
+			data_files = listdir(images_dir)[int(num_examples*0.8):]
 
-			image_id = filename[:-4]
+		# add each example to the dataset (image and xml boundary information)
+		for filename in data_files:
 
-			if image_id in ['00090']: # skip bad images # TODO: what are these?
-				continue
-			# skip all images after 150 if we are building the train set # TODO: why?
-			if is_train and int(image_id) >= 150:
-				continue
-			# skip all images before 150 if we are building the test/val set
-			if not is_train and int(image_id) < 150:
-				continue
+			image_id = path.splitext(filename)[0]
 			img_path = images_dir + filename
 			ann_path = annotations_dir + image_id + '.xml'
 			self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
 
 	def extract_boxes(self, filename):
-                """
-                Extract bounding boxes from an annotation file
-                """
+		"""
+		Extract bounding boxes from an annotation file
+		"""
 		tree = ElementTree.parse(filename)
 		root = tree.getroot()
 		boxes = list()
@@ -66,17 +66,22 @@ class PlansDataset(Dataset):
 			coors = [xmin, ymin, xmax, ymax]
 			boxes.append(coors)
 
+		names = list()
+		for name in root.findall('.//object'):
+			n = name.find('name').text
+			names.append(n)
+
 		width = int(root.find('.//size/width').text)
 		height = int(root.find('.//size/height').text)
-		return boxes, width, height
+		return boxes, width, height, names
 
 	def load_mask(self, image_id):
-                """
-                Load the masks for an image
-                """
+		"""
+		Load the masks for an image
+		"""
 		info = self.image_info[image_id]
 		path = info['annotation']
-		boxes, w, h = self.extract_boxes(path)
+		boxes, w, h, names = self.extract_boxes(path)
 		masks = zeros([h, w, len(boxes)], dtype='uint8') # create one array for all masks, each on a different channel
 
 		class_ids = list()
@@ -85,35 +90,33 @@ class PlansDataset(Dataset):
 			row_s, row_e = box[1], box[3]
 			col_s, col_e = box[0], box[2]
 			masks[row_s:row_e, col_s:col_e, i] = 1
-			class_ids.append(self.class_names.index('plans'))
+			class_ids.append(self.class_names.index(names[i]))
 		return masks, asarray(class_ids, dtype='int32')
 
 	def image_reference(self, image_id):
-                """
-                Load an image reference
-                """
+		"""
+		Load an image reference
+		"""
 		info = self.image_info[image_id]
 		return info['path']
 
 # Configuration file for analysis
 class PlansConfig(Config):
 	NAME = "plans_cfg"
-        raise NotImplementedError("You need to specify the number of classes!")
-	NUM_CLASSES = 1 + 1 # Number of classes (background + ?)
+	NUM_CLASSES = 3 + 1 # Number of classes (background + ?)
 	STEPS_PER_EPOCH = 131
         
 # Configuration file for evaluation
 class PredictionConfig(Config):
 	NAME = "plans_cfg"
-        raise NotImplementedError("You need to specify the number of classes!")
-	NUM_CLASSES = 1 + 1 	# number of classes (background + ?)
+	NUM_CLASSES = 3 + 1 	# number of classes (background + ?)
 	GPU_COUNT = 1
 	IMAGES_PER_GPU = 1
 
 def evaluate_model(dataset, model, cfg):
-        """
-        Calculate the mAP for a model on a given dataset
-        """
+	"""
+	Calculate the mAP for a model on a given dataset
+	"""
 	APs = list()
 	for image_id in dataset.image_ids:
 
@@ -125,14 +128,14 @@ def evaluate_model(dataset, model, cfg):
 		r = yhat[0] # extract results for first sample
 		AP, _, _, _ = compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r['masks'])
 		APs.append(AP)
-
-        mAP = mean(APs)
+		
+	mAP = mean(APs)
 	return mAP
 
 def plot_actual_vs_predicted(dataset, model, cfg, n_images=5):
-        """
-        Plot a number of photos with ground truth and predictions
-        """
+	"""
+	Plot a number of photos with ground truth and predictions
+	"""
 	for i in range(n_images):
 		image = dataset.load_image(i)
 		mask, _ = dataset.load_mask(i)
