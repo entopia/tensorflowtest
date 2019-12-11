32dae420a56c21646dbff71f048a2084078a061e
Anna Hadjitofi <annahadji@users.noreply.github.com>
2019-12-11 14:27:01 +0000

    Documentation: remove/add comments

--- obj_detection/objd.py
+++ obj_detection/objd.py
@@ -13,22 +13,35 @@ from mrcnn.utils import compute_ap
 from mrcnn.model import load_image_gt
 from mrcnn.model import mold_image
 
+
+"""
+
+This file contains classes and helper functions used to train and evaluate
+a keras objection detection model using transfer learning.
+
+IMPORTANT!
+The code in this file is largely refactored from the following tutorial:
+https://machinelearningmastery.com/how-to-train-an-object-detection-model-with-keras/
+
+"""
+
 class PlansDataset(Dataset):
-	# load the dataset definitions
+
 	def load_dataset(self, dataset_dir, is_train=True):
 		# define one class
 		self.add_class("dataset", 1, "plans")
-		# define data locations
+		
 		images_dir = dataset_dir + '/images/'
 		annotations_dir = dataset_dir + '/annots/'
-		# find all images
+
+                # add each example to the dataset (image and xml boundary information)
 		for filename in listdir(images_dir):
-			# extract image id
+
 			image_id = filename[:-4]
-			# skip bad images
-			if image_id in ['00090']:
+
+			if image_id in ['00090']: # skip bad images # TODO: what are these?
 				continue
-			# skip all images after 150 if we are building the train set
+			# skip all images after 150 if we are building the train set # TODO: why?
 			if is_train and int(image_id) >= 150:
 				continue
 			# skip all images before 150 if we are building the test/val set
@@ -36,16 +49,14 @@ class PlansDataset(Dataset):
 				continue
 			img_path = images_dir + filename
 			ann_path = annotations_dir + image_id + '.xml'
-			# add to dataset
 			self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
 
-	# extract bounding boxes from an annotation file
 	def extract_boxes(self, filename):
-		# load and parse the file
+                """
+                Extract bounding boxes from an annotation file
+                """
 		tree = ElementTree.parse(filename)
-		# get the root of the document
 		root = tree.getroot()
-		# extract each bounding box
 		boxes = list()
 		for box in root.findall('.//bndbox'):
 			xmin = int(box.find('xmin').text)
@@ -54,114 +65,95 @@ class PlansDataset(Dataset):
 			ymax = int(box.find('ymax').text)
 			coors = [xmin, ymin, xmax, ymax]
 			boxes.append(coors)
-		# extract image dimensions
+
 		width = int(root.find('.//size/width').text)
 		height = int(root.find('.//size/height').text)
 		return boxes, width, height
 
-	# load the masks for an image
 	def load_mask(self, image_id):
-		# get details of image
+                """
+                Load the masks for an image
+                """
 		info = self.image_info[image_id]
-		# define box file location
 		path = info['annotation']
-		# load XML
 		boxes, w, h = self.extract_boxes(path)
-		# create one array for all masks, each on a different channel
-		masks = zeros([h, w, len(boxes)], dtype='uint8')
-		# create masks
+		masks = zeros([h, w, len(boxes)], dtype='uint8') # create one array for all masks, each on a different channel
+
 		class_ids = list()
 		for i in range(len(boxes)):
 			box = boxes[i]
 			row_s, row_e = box[1], box[3]
 			col_s, col_e = box[0], box[2]
 			masks[row_s:row_e, col_s:col_e, i] = 1
-			class_ids.append(self.class_names.index('kangaroo'))
+			class_ids.append(self.class_names.index('plans'))
 		return masks, asarray(class_ids, dtype='int32')
 
-	# load an image reference
 	def image_reference(self, image_id):
+                """
+                Load an image reference
+                """
 		info = self.image_info[image_id]
 		return info['path']
 
-# define a configuration for the model
+# Configuration file for analysis
 class PlansConfig(Config):
-	# Give the configuration a recognizable name
 	NAME = "plans_cfg"
-	# Number of classes (background + kangaroo)
-	NUM_CLASSES = 1 + 1
-	# Number of training steps per epoch
+        raise NotImplementedError("You need to specify the number of classes!")
+	NUM_CLASSES = 1 + 1 # Number of classes (background + ?)
 	STEPS_PER_EPOCH = 131
         
-# define the prediction configuration
+# Configuration file for evaluation
 class PredictionConfig(Config):
-	# define the name of the configuration
 	NAME = "plans_cfg"
-	# number of classes (background + kangaroo)
-	NUM_CLASSES = 1 + 1
-	# simplify GPU config
+        raise NotImplementedError("You need to specify the number of classes!")
+	NUM_CLASSES = 1 + 1 	# number of classes (background + ?)
 	GPU_COUNT = 1
 	IMAGES_PER_GPU = 1
 
-# calculate the mAP for a model on a given dataset
 def evaluate_model(dataset, model, cfg):
+        """
+        Calculate the mAP for a model on a given dataset
+        """
 	APs = list()
 	for image_id in dataset.image_ids:
 
-                # load image, bounding boxes and masks for the image id
 		image, image_meta, gt_class_id, gt_bbox, gt_mask = load_image_gt(dataset, cfg, image_id, use_mini_mask=False)
-		# convert pixel values (e.g. center)
-		scaled_image = mold_image(image, cfg)
-		# convert image into one sample
-		sample = expand_dims(scaled_image, 0)
-		# make prediction
-		yhat = model.detect(sample, verbose=0)
-		# extract results for first sample
-		r = yhat[0]
-		# calculate statistics, including AP
+		scaled_image = mold_image(image, cfg) # convert pixel values (e.g. center)
+		sample = expand_dims(scaled_image, 0) # convert image into one sample
+		
+		yhat = model.detect(sample, verbose=0) # make prediction
+		r = yhat[0] # extract results for first sample
 		AP, _, _, _ = compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r['masks'])
-		# store
 		APs.append(AP)
-	# calculate the mean AP across all images
-	mAP = mean(APs)
+
+        mAP = mean(APs)
 	return mAP
 
-# plot a number of photos with ground truth and predictions
 def plot_actual_vs_predicted(dataset, model, cfg, n_images=5):
-	# load image and mask
+        """
+        Plot a number of photos with ground truth and predictions
+        """
 	for i in range(n_images):
-		# load the image and mask
 		image = dataset.load_image(i)
 		mask, _ = dataset.load_mask(i)
-		# convert pixel values (e.g. center)
-		scaled_image = mold_image(image, cfg)
-		# convert image into one sample
-		sample = expand_dims(scaled_image, 0)
-		# make prediction
+		scaled_image = mold_image(image, cfg) # convert pixel values (e.g. center)
+		sample = expand_dims(scaled_image, 0) # convert image into one sample
+
 		yhat = model.detect(sample, verbose=0)[0]
-		# define subplot
 		pyplot.subplot(n_images, 2, i*2+1)
-		# plot raw pixel data
 		pyplot.imshow(image)
 		pyplot.title('Actual')
-		# plot masks
+                
 		for j in range(mask.shape[2]):
 			pyplot.imshow(mask[:, :, j], cmap='gray', alpha=0.3)
-		# get the context for drawing boxes
+
 		pyplot.subplot(n_images, 2, i*2+2)
-		# plot raw pixel data
 		pyplot.imshow(image)
 		pyplot.title('Predicted')
 		ax = pyplot.gca()
-		# plot each box
 		for box in yhat['rois']:
-			# get coordinates
 			y1, x1, y2, x2 = box
-			# calculate width and height of the box
 			width, height = x2 - x1, y2 - y1
-			# create the shape
 			rect = Rectangle((x1, y1), width, height, fill=False, color='red')
-			# draw the box
 			ax.add_patch(rect)
-	# show the figure
 	pyplot.show()
